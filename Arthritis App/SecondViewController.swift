ec70938b40c4c4e1486a7600813535f4aa010d08
Danielle R <ribeirodanielle26@gmail.com>
2020-10-22 22:03:58 -0400

    Initial Commit

---
+++ Arthritis App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Arthritis App
+//
+//  Created by Danielle Ribeiro on 2020-10-22.
+//  Copyright © 2020 Danielle Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
